335b7485e53012146fa06f9eb2a34b63e8fba4ab
Maxafangsco <39309700+Maxafangsco@users.noreply.github.com>
2021-04-03 18:28:25 +0400

    This is the first working model of the website

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,102 @@
+<footer class="bg-gray-800 py-20 mt-20">
+    <div class="sm:grid grid-cols-3 w-4/5 pb-10 m-auto border-b-2 border-gray-700">
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Pages
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="/blog">
+                        Blog
+                    </a>
+                </li>
+
+                <li>
+
+                    <a href="/login">
+                        Login
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/register">
+                        Register
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Find Us
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        what we do
+                    </a>
+                </li>
+                <li>
+                    <a href="/">
+                        Address
+                    </a>
+                </li>
+
+                <li>
+
+                    <a href="/">
+                        Phone
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/register">
+                        Contact
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3 class="text-l sm:font-bold text-gray-100">
+                Latest Post
+            </h3>
+
+            <ul class="py-4 sm:text-s pt-4 text-gray-400">
+                <li class="pb-1">
+                    <a href="/">
+                        Why we love tech
+                    </a>
+                </li>
+                <li>
+                    <a href="/blog">
+                        why we love design
+                    </a>
+                </li>
+
+                <li>
+
+                    <a href="/login">
+                        why we learn laravel
+                    </a>
+                </li>
+
+                <li>
+                    <a href="/register">
+                        Why php is the best
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <p class="w-25 w-4/5 pb-3 m-auto text-xs text-gray-100 pt-6">
+        Copyright 2017-2021 code with Maxafangsco. All Rights Reserved
+    </p>
+</footer>
